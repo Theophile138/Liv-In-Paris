610bc2340616b475259d9617a72a20676e590903
Theophile138 <theophile@phenix27.homedns.org>
2025-01-30 11:39:00 +0100

    Ajout des classes

---
+++ Liv-In-Paris/Lien.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liv_In_Paris
+{
+    internal class Lien
+    {
+    }
+}
