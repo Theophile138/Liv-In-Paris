e629ea8d39281443bab27dfb10f21eff3e1b885b
Theophile138 <theophile@phenix27.homedns.org>
2025-01-30 11:04:31 +0100

    Ajoutez des fichiers projet.

---
+++ Liv-In-Paris/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Liv_In_Paris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
