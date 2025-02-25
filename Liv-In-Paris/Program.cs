49572648217785b03a05b0a31b4c7cbcb2df2087
Louis LABOUREAU <louis.laboureau@edu.devinci.fr>
2025-02-25 22:59:32 +0100

    fsf

--- Liv-In-Paris/Program.cs
+++ Liv-In-Paris/Program.cs
@@ -4,11 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Salut !");
+            // Console.WriteLine("Salut !");
 
-            Graphe myGraphe = Fichier.LoadGraph("soc-karate.mtx");
+            // Graphe myGraphe = Fichier.LoadGraph("soc-karate.mtx");
 
-            Console.WriteLine("Fini");
+            //Console.WriteLine("Fini");
+            int[,] tab2D = new int[3, 2]{
+                               {3, 3},
+                               {1, 1},
+                               {2, 2}
+                            };
+            int[,] tab = Graphe.Matriceadj(tab2D);
+            Graphe.affichermatriceadj(tab, tab2D);
+            Graphe.Listeadjacence(tab2D);
+
         }
     }
 }
