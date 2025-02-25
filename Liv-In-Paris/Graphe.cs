4db904dd5314e13d02befda30283745608df3b61
Louis LABOUREAU <louis.laboureau@edu.devinci.fr>
2025-02-25 16:07:19 +0100

    ff

--- Liv-In-Paris/Graphe.cs
+++ Liv-In-Paris/Graphe.cs
@@ -8,5 +8,21 @@ namespace Liv_In_Paris
 {
     internal class Graphe
     {
+        public int[][] Matriceadj(int[][] t) {
+            
+
+
+
+        }
+        public static int getnbrnoeud(int[][] t) {
+            int compteur = 0;
+            for (int i = 0; i < t.GetLength(1); i++){
+                if (t[1][i] > compteur)
+                {
+                    compteur = t[1][i];
+                }            
+            }
+            return compteur;
+        }
     }
 }
