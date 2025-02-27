4db1ef0455e92231cb64fecf25420917d3055114
Theophile138 <theophile@phenix27.homedns.org>
2025-02-27 09:07:19 +0100

    console

--- Liv-In-Paris/Program.cs
+++ Liv-In-Paris/Program.cs
@@ -1,21 +1,30 @@
-﻿namespace Liv_In_Paris
+﻿using System.Runtime.InteropServices;
+
+namespace Liv_In_Paris
 {
     internal class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern bool AllocConsole();
+
         [STAThread]
         static void Main(string[] args)
         {
             // Console.WriteLine("Salut !");
 
-            // Graphe myGraphe = Fichier.LoadGraph("soc-karate.mtx");
+            AllocConsole(); // Ouvre la console
+            Console.WriteLine("Console activée !");
+
+            Graphe myGraphe = Fichier.LoadGraph("soc-karate.mtx");
 
             //Console.WriteLine("Fini");
+            
             int[,] tab2D = new int[3, 2]{
                                {3, 3},
                                {1, 1},
                                {2, 2}
                             };
-            int[,] tab = Graphe.Matriceadj(tab2D);
+            int[,] tab = Graphe.Matriceadj();
             Graphe.affichermatriceadj(tab, tab2D);
             Graphe.Listeadjacence(tab2D);
 
